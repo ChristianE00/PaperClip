450ed75936b9805187be8d7c939d4709b48ed7d8
Christian Ewing <u1273549@utah.edu>
2024-10-11 19:04:38 -0600

    Clipboard manager saves session clips & ui adjustments

--- paperClip/Form1.cs
+++ paperClip/Form1.cs
@@ -16,7 +16,7 @@ namespace paperClip
     public partial class Form1 : Form
     {
         private SharpClipboard clipboard;
-        private TextBox clipboardTextBox;
+        //private TextBox clipboardTextBox;
         private FlowLayoutPanel flowLayoutPanel;
         private ArrayList textBoxList = new ArrayList();
 
@@ -37,20 +37,7 @@ namespace paperClip
             flowLayoutPanel.AutoScroll = true;
             flowLayoutPanel.Dock = DockStyle.Fill; // Make the panel fill the form
             this.Controls.Add(flowLayoutPanel);
-
-            // Initialize TextBox
-            clipboardTextBox = new TextBox();
-            clipboardTextBox.ReadOnly = true;
-            clipboardTextBox.BorderStyle = BorderStyle.None;
-            clipboardTextBox.Multiline = true;
-            clipboardTextBox.WordWrap = true;
-            clipboardTextBox.ScrollBars = ScrollBars.Vertical;
-            clipboardTextBox.Width = flowLayoutPanel.ClientSize.Width - 20; // Adjust the width as needed
-            //clipboardTextBox.Height = 100; // Set a fixed height for the TextBox
-            flowLayoutPanel.Controls.Add(clipboardTextBox);
-
-            // Handle form resize to adjust TextBox size
-            this.Resize += new EventHandler(Form1_Resize);
+            
         }
         private void CreateNewTextBox(string text)
         {
@@ -63,16 +50,17 @@ namespace paperClip
             tBox.Width = flowLayoutPanel.ClientSize.Width - 20; // Adjust the width as needed
             tBox.Height = generateTextBoxHeight(text, tBox); // Set a fixed height for the TextBox
             tBox.Text = text;
+            tBox.BackColor = Color.Gray;
             flowLayoutPanel.Controls.Add(tBox);
             textBoxList.Add(tBox);
+            flowLayoutPanel.Controls.Add(tBox);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // Load the first textbox
             string clipboardText = Clipboard.GetText();
-            clipboardTextBox.Height = generateTextBoxHeight(clipboardTextBox.Text, clipboardTextBox);
-            clipboardTextBox.Text = "Your current clipboard item: " + clipboardText;
+
         }
 
         private void ClipboardChanged(object sender, ClipboardChangedEventArgs e)
@@ -81,25 +69,23 @@ namespace paperClip
             if (e.ContentType == SharpClipboard.ContentTypes.Text)
             {
                 string clipboardText = clipboard.ClipboardText;
-                Console.WriteLine("Clipboard Text: " + clipboardText);
-                int height = generateTextBoxHeight(clipboardText, clipboardTextBox);
-                clipboardTextBox.Height = height;
-                clipboardTextBox.Text = "Clipboard Text: " + clipboardText;
-                
+                CreateNewTextBox(clipboardText);             
             }
         }
 
         private int generateTextBoxHeight(string text, TextBox box)
         {
-            int height = TextRenderer.MeasureText(text, clipboardTextBox.Font, new Size(clipboardTextBox.Width, 0),
+            int height = TextRenderer.MeasureText(text, box.Font, new Size(box.Width, 0),
                     TextFormatFlags.WordBreak | TextFormatFlags.TextBoxControl).Height;
             height = (height > flowLayoutPanel.ClientSize.Height) ? flowLayoutPanel.ClientSize.Height - 10 : height;
             return height;
         }
 
+        /*
         private void Form1_Resize(object sender, EventArgs e)
         {
             clipboardTextBox.Width = flowLayoutPanel.ClientSize.Width - 20;
         }
+        */
     }
 }
